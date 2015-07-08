732c0d81730cb41968fc16a58b14f43544cc1976
Edward A Maxedon Sr <edadma@gmail.com>
2015-07-07 20:40:24 -0400

    start handling of large values

---
+++ src/main/scala/AllocIO.scala
@@ -0,0 +1,8 @@
+package ca.hyperreal.bittydb
+
+
+class AllocIO( parent: IO, pointer: Long = 0 ) extends MemIO {
+	def done( append: Boolean ) {
+		
+	}
+}
---
+++ src/main/scala/ExpandableByteBuffer.scala
@@ -0,0 +1,47 @@
+package ca.hyperreal.bittydb
+
+import java.nio.ByteBuffer
+
+
+class ExpandableByteBuffer
+{
+	private var array: Array[Byte] = null
+	private var _buffer: ByteBuffer = null
+	private var _size = 0
+	
+	array = allocate( 16 )
+	
+	def buffer = _buffer
+	
+	def size = _size
+	
+	def size_=( s: Int ) {
+		sizeHint( s )
+		_size = s
+	}
+	
+	def allocate( capacity: Int ) = {
+		val res = new Array[Byte]( capacity )
+		val p = 
+			if (_buffer eq null)
+				0
+			else
+				_buffer.position
+				
+		_buffer = ByteBuffer.wrap( res )
+		_buffer.position( p )
+		res
+	}
+	
+	def need( bytes: Int ) = sizeHint( _buffer.position + bytes )
+	
+	def sizeHint( len: Int ) {
+		if (len > array.length && len >= 1) {
+			val newarray = allocate( len*2 )
+			compat.Platform.arraycopy( array, 0, newarray, 0, _size )
+			array = newarray
+		}
+		
+		_size = _size max len
+	}
+}
