5ea7581f3f733726ead0877739b29ca84d67a19b
Edward A Maxedon <edadma@gmail.com>
2017-04-28 06:54:28 -0400

    reproducible

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -10,17 +10,20 @@ object Main extends App {
 	val coll = db( "test" )
 	val array = new ArrayBuffer[Map[String, Any]]
 
-	for (_ <- 1 to 10) {
-		val m = rndObject
+	db.io.dump
+
+	for (_ <- 1 to 1) {
+		val m = Map("aaaaaaaaa" -> 0x55aa55aa)//rndObject
 
 		coll.insert( m )
 		array += m
 	}
 
+	db.io.dump
 	println( coll.set == array.toSet )
 	println( db.length )
 
-	for (_ <- 1 to 5) {
+	for (_ <- 1 to 1) {
 		val ind = nextInt( array.length )
 		val doc = array( ind )
 
@@ -31,21 +34,22 @@ object Main extends App {
 	println( coll.set == array.toSet )
 	println( db.length )
 
-	for (_ <- 1 to 5) {
-		val m = rndObject
-
-		coll.insert( m )
-		array += m
-	}
-
-	println( coll.set == array.toSet )
-	println( db.length )
+//	for (_ <- 1 to 1) {
+//		val m = Map("aaaaaaaaaaaaaaa" -> 0x55aa55aa)//rndObject
+//
+//		println( m )
+//		coll.insert( m )
+//		array += m
+//	}
+//
+//	println( coll.set == array.toSet )
+//	println( db.length )
 
 	db.close
 
 	def rndAlpha = new String( Array.fill( nextInt(17) )((nextInt('z' - 'a') + 'a').toChar) )
 
-	def rndObject =
+	def rndObject = //Map( "abababababababab" -> 0x5a5a5a5a )
 		Map(
 			(for (_ <- 0 to nextInt( 1 ))
 				yield
