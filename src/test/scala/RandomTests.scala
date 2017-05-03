5ee97d3f27670f8139bfee481ae4097b33740593
Edward A Maxedon <edadma@gmail.com>
2017-05-03 11:04:55 -0400

    improve random test

--- src/test/scala/RandomTests.scala
+++ src/test/scala/RandomTests.scala
@@ -1,6 +1,6 @@
 package xyz.hyperreal.bittydb
 
-import collection.mutable.ArrayBuffer
+import collection.mutable.{HashSet, ArrayBuffer}
 
 import scala.util.Random._
 
@@ -11,18 +11,28 @@ import prop.PropertyChecks
 
 class RandomTests extends FreeSpec with PropertyChecks with Matchers {
 
-	def rndAlpha = new String( Array.fill( nextInt(17) )((nextInt('z' - 'a') + 'a').toChar) )
+	def rndAlpha = new String( Array.fill( nextInt(10) )((nextInt('z' - 'a') + 'a').toChar) )
+
+	def rnd( s: collection.Set[Map[String, Any]] ): Map[String, Any] = {
+		val a = Map( rndAlpha -> nextInt (Int.MaxValue) )
+
+		if (s(a))
+			rnd( s )
+		else
+			a
+	}
 
 	val ints = for (n <- Gen.choose(1, 10)) yield n
 
 	"mem" in {
 		forAll (ints) { _ =>
-			val db = Connection.mem( 'uuid -> false )
+			val db = Connection.mem( 'uuid -> false, 'pwidth -> 2, 'cwidth -> 2 )
 			val coll = db( "test" )
 			val array = new ArrayBuffer[Map[String, Any]]
+			val set = new HashSet[Map[String, Any]]
 
 			for (_ <- 1 to 100) {
-				val m = Map( rndAlpha -> nextInt(Int.MaxValue) )
+				val m = rnd( set )
 
 				coll.insert( m )
 				array += m
@@ -36,12 +46,13 @@ class RandomTests extends FreeSpec with PropertyChecks with Matchers {
 
 				array remove ind
 				coll remove doc
+				set -= doc
 			}
 
 			coll.set shouldEqual array.toSet
 
 			for (_ <- 1 to 50) {
-				val m = Map( rndAlpha -> nextInt(Int.MaxValue) )
+				val m = rnd( set )
 
 				coll.insert( m )
 				array += m
