d20bfd41c144007a7bae61694d517b083889b399
Edward A Maxedon <edadma@gmail.com>
2017-05-19 18:19:27 -0400

    fix Blob bytes() method

--- src/main/scala/Blob.scala
+++ src/main/scala/Blob.scala
@@ -1,36 +1,32 @@
 package xyz.hyperreal.bittydb
 
-import java.io.{ByteArrayInputStream, InputStream}
+import java.io.{ByteArrayInputStream, DataInputStream, InputStream}
 
 
 abstract class Blob {
 	def length: Long
+
 	def stream: InputStream
-	def bytes( offset: Long, length: Int ): Array[Byte]
+
+	def bytes( offset: Long, length: Int ) = {
+		val res = new Array[Byte]( length )
+		val s = new DataInputStream( stream )
+
+		s.skip( offset )
+		s.readFully( res )
+		s.close
+		res
+	}
 }
 
 class ArrayBlob( a: Array[Byte] ) extends Blob {
-	val stream = new ByteArrayInputStream( a )
 	val length = a.length
 
-	def bytes( offset: Long, length: Int ) = a.slice( offset.toInt, offset.toInt + length )
+	def stream = new ByteArrayInputStream( a )
+
+	override def bytes( offset: Long, length: Int ) = a.slice( offset.toInt, offset.toInt + length )
 }
 
 class IOBlob( io: IO, addr: Long, val length: Long ) extends Blob {
 	def stream = io.byteInputStream( addr, length )
-
-	def bytes( offset: Long, length: Int ) = {
-		val res = new Array[Byte]( length )
-		val s = stream
-
-		s.skip( offset )
-
-		s.read( res ) match {
-			case `length` =>
-			case l => sys.error( s"wrong number of bytes read: $l" )
-		}
-
-		s.close
-		res
-	}
 }
